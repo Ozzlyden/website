31264f1b900d46c73285c58367ce121066bdc4a9
Victor Valente <Valente-victor@hotmail.com>
2023-05-30 02:33:22 -0300

    Ultimos ajustes CSS

--- web_site/index.php
+++ web_site/index.php
@@ -79,7 +79,7 @@
         <h2 class="title">Especialidades</h2>
             <div class="w33 left box-especialidade">
                 <h3><i class="fa-brands fa-css3-alt"></i></h3>
-                <h3>Icone CSS3</h3>
+                <h4>CSS3</h4>
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                      sed do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
@@ -92,7 +92,7 @@
             </div>
             <div class="w33 left box-especialidade">
                 <h3><i class="fa-brands fa-html5"></i></h3>
-                <h3>Icone HTML5</h3>
+                <h4>HTML5</h4>
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                      sed do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
@@ -105,7 +105,7 @@
             </div>
             <div class="w33 left box-especialidade">
                 <h3><i class="fa-brands fa-square-js"></i></h3>
-                <h3>Icone JavaScript</h3>
+                <h4>JavaScript</h4>
                 <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                      sed do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
@@ -116,13 +116,14 @@
                     officia deserunt mollit anim id est laborum."
                 </p>
             </div>
+        <div class="clear"></div>
         </div> 
     </section>
 
     <section class="extras">
         <div class="center">
-            <div class="w50 left">
-            <h2 class="title">Depoimentos</h2>
+            <div class="w50 left depoimentos-container">
+            <h2 class="title">Depoimentos dos nossos clientes</h2>
                 <div class="depoimentos-single">
                     <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore magna
@@ -133,49 +134,43 @@
                         sint occaecat cupidatat non proident, sunt in culpa qui
                         officia deserunt mollit anim id est laborum."
                     </p>
-                    <p class="nome-autor">Lorem Ipsum</p>
+                    <p class="nome-autor">Nome Cliente</p>
+                </div>
+                <div class="depoimentos-single">
+                    <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                        officia deserunt mollit anim id est laborum."
+                    </p>
+                    <p class="nome-autor">Nome Cliente</p>
                 </div>
                 <div class="depoimentos-single">
                     <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
                         sed do eiusmod tempor incididunt ut labore et dolore magna
                         aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                        sint occaecat cupidatat non proident, sunt in culpa qui
-                        officia deserunt mollit anim id est laborum."
+                        ull officia deserunt mollit anim id est laborum."
                     </p>
-                    <p class="nome-autor">Lorem Ipsum</p>
-                </div>
-                <div class="depoimentos-single">
-                    <p class="depoimento-descricao">"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                        sint occaecat cupidatat non proident, sunt in culpa qui
-                        officia deserunt mollit anim id est laborum."
-                    </p>
-                    <p class="nome-autor">Lorem Ipsum</p>
+                    <p class="nome-autor">Nome Cliente</p>
                 </div>
             </div>
-            <div class="w50 left"> <!--w50-->
+            <div class="w50 left servicos-container"> <!--w50-->
             <h2 class="title">Serviços</h2>
                 <div class="servicos">
                     <ul>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
+                        <li>Serviço 1</li>
+                        <li>Serviço 2</li>
+                        <li>Serviço 3</li>
                     </ul>
                 </div>
             </div>
-            <div class="cleear"></div>
+            <div class="clear"></div>
         </div>
     </section>
 
-    <footer class="center"> <!--center-->
+    <footer>
+        <div class="center"> <!--center-->
         <p>Todos os direitos reservados</p>
+        </div>
     </footer>
 
 </body>
