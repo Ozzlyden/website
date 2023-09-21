c7fb7d1c816ff34cf838d363901ccb7b72ce9b11
Victor Valente <Valente-victor@hotmail.com>
2023-09-21 16:03:59 -0300

    Correcoes e desenvolvendo box painel

--- painel/main.php
+++ painel/main.php
@@ -21,50 +21,83 @@
 <body>
 
 <div class="menu">
-
     <div class="menu-wraper">
-    <div class="box-usuario">
-    <?php
-        if($_SESSION['img'] == ''){
-    ?>
-         <div class="avatar-usuario">
-            <i class="fa-regular fa-user"></i>
-        </div><!--avatar-usuario-->
-        <?php }else { ?>
+        <div class="box-usuario">
+            <?php
+                if($_SESSION['img'] == ''){
+            ?>
+            <div class="avatar-usuario">
+                <i class="fa-regular fa-user"></i>
+            </div><!--avatar-usuario-->
+            <?php }else { ?>
             <div class="img-usuario">
-            <img src="<?php echo INCLUDE_PATH_PAINEL ?> upload/ <?php echo $_SESSION['img']; ?>"
-        </div><!--img-usuario-->
-    <?php } ?>    
-        
+                <img src="<?php echo INCLUDE_PATH_PAINEL ?> upload/ <?php echo $_SESSION['img']; ?>">
+            </div><!--img-usuario-->
+        <?php } ?>    
+            
+            <div class="nome-usuario">
+                <p><?php echo $_SESSION['nome']; ?></p>
+                <p><?php echo pegaCargo($_SESSION['cargo']); ?> </p>
+            </div><!--nome-usuario-->
+        </div><!--box-usuario-->
 
-        <div class="nome-usuario">
-            <p><?php echo $_SESSION['nome']; ?></p>
-            <p><?php echo pegaCargo($_SESSION['cargo']); ?> </p>
-        </div><!--nome-usuario-->
-    </div><!--box-usuario-->
+        <div class="items-menu">
+            <h2>Cadastro</h2>
+                <a href="">Cadastrar Depoimento</a>
+                <a href="">Cadastrar Serviço</a>
+                <a href="">Cadastrar Slides</a>
+            <h2>Gestão</h2>
+                <a href="">Listar Depoimentos</a>
+                <a href="">Listar Serviços</a>
+                <a href="">Listar Slides</a>
+            <h2>Administração do painel</h2>
+                <a href="">Editar Usuário</a>
+            <h2>Configuração Geral</h2>
+                <a href="">Editar Site</a>
+	    </div><!--items-menu-->
     </div><!--menu-wraper-->
-
 </div><!--menu-->
 
 <header>
-
     <div class="center">
+
         <div class="menu-btn">
             <i class="fa-solid fa-bars"></i>
-
         </div><!--menu-btn-->
+
         <div class="loggout">
             <a href="<?php echo INCLUDE_PATH_PAINEL; ?>?loggout"><i class="fa-solid fa-right-from-bracket"></i><span> Sair  </span></a>
         </div> <!--loggout-->
     </div><!--center-->
 
     <div class="clear"></div>
-
 </header>
 
 <div class="content">
-
         <div class="box-content left w100">
+            <h2><i class="fa-solid fa-house-user"></i>Painel de Controle</h2>
+
+            <div class="box-metricas">
+                <div class="box-metrica-single">
+                    <div class="box-metrica-wraper">
+                        <h2>Usuário Online</h2>
+                        <p>10</p>
+                    </div><!--box-metrica-wraper-->
+                </div><!--box-metrica-singel-->
+                <div class="box-metrica-single">
+                    <div class="box-metrica-wraper">
+                        <h2>Total de Visitaas</h2>
+                        <p>100</p>
+                    </div><!--box-metrica-wraper-->
+                </div><!--box-metrica-singel-->
+                <div class="box-metrica-single">
+                    <div class="box-metrica-wraper">
+                        <h2>Visitas Hoje</h2>
+                        <p>3</p>
+                    </div><!--box-metrica-wraper-->
+                </div><!--box-metrica-singel-->
+                <div class="clear"></div>
+            </div><!--box-metrica-->
 
         </div> <!--box-content-->
         
